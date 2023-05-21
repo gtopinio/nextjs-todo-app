378abdd4b1d0437820234deb429dc1b84315e751
gtopinio <mctopinio@up.edu.ph>
2023-05-21 23:56:43 +0800

    update: add flex and justify-start properties to app title

--- src/app/AppNavBar.js
+++ src/app/AppNavBar.js
@@ -1,8 +1,8 @@
 export default function AppNavBar() {
     return (
-        <div>
+      <div>
         <nav className="bg-cyan-950">
-          <div className="max-w-7xl mx-auto px-4 py-2">
+          <div className="max-w-7xl mx-auto px-4 py-2 flex justify-start">
             <button className="rounded-3xl px-4 py-2 bg-cyan-600 hover:bg-cyan-700">
               <span className="text-white text-lg font-bold">NextJS To-Do List</span>
             </button>
@@ -10,4 +10,5 @@ export default function AppNavBar() {
         </nav>
       </div>
     );
-}
+  }
+  

9c7edb8916306675b2ad6129f58d7bf7279651c7
gtopinio <mctopinio@up.edu.ph>
2023-05-21 23:51:39 +0800

    feature: add task input component

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,17 @@
+"use client"
+
+import TaskInput from "./TaskInput";
+
 export default function Home() {
+  const handleAddTask = (taskText) => {
+    // Handle adding the task to your array or perform any desired action
+    console.log('New task:', taskText);
+  };
+
   return (
     <main>
-      <div className="bg-gray-800 min-h-screen">
-        {/* Your content goes here */}
+      <div className="bg-gray-800 min-h-screen pt-16">
+        <TaskInput onAddTask={handleAddTask}/>
       </div>
     </main>
   );

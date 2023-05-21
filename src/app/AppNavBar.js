aebc5a616ec1bc72cabc1258f3f80ce1153d262f
gtopinio <mctopinio@up.edu.ph>
2023-05-21 22:57:41 +0800

    feature: add a custom navbar to app layout that has the app title

---
+++ src/app/AppNavBar.js
@@ -0,0 +1,13 @@
+export default function AppNavBar() {
+    return (
+        <div>
+        <nav className="bg-cyan-950">
+          <div className="max-w-7xl mx-auto px-4 py-2">
+            <button className="rounded-3xl px-4 py-2 bg-cyan-600 hover:bg-cyan-700">
+              <span className="text-white text-lg font-bold">NextJS To-Do List</span>
+            </button>
+          </div>
+        </nav>
+      </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="title">
-        <h1>Home Page</h1>
+      <div>
       </div>
     </main>
   )
